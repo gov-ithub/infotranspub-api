dd3608ee316c4a585f792146262db21b1ace7da7
Alex Butum <alexbutum@linux.com>
2016-11-01 17:25:28 +0200

    Add Convert GTFS to SQL subproject and usage script

---
+++ sql/convert_gtfs_to_sql.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -u
+
+# Usage: ./convert_gtfs_to_sql.sh "$HOME/Projects/Me/GovITHub/ratbv-to-gtfs-converter/old_data/brasov/"
+
+GTFS_CONVERTER_HOME="gtfs_to_sql_converter/src"
+INPUT_GTFS_FEED="$1"
+
+pushd "$GTFS_CONVERTER_HOME"
+
+cat gtfs_tables.sql \
+    <(python2 import_gtfs_to_sql.py "$INPUT_GTFS_FEED" ) \
+    gtfs_tables_makespatial.sql \
+    gtfs_tables_makeindexes.sql \
+    vacuumer.sql > ../../out_postgres.sql
+    
+popd
