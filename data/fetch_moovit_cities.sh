927b448e6c89b8d24e774472ce8349c1cc9d2d18
Alex Butum <alexbutum@linux.com>
2016-11-01 19:17:44 +0200

    Use CSV for Moovit cities output. Show count

--- data/fetch_moovit_cities.sh
+++ data/fetch_moovit_cities.sh
@@ -2,5 +2,9 @@
 
 set -ue
 
-xidel -q -e "//*/div/article/ul/li[47]/span[contains(@class, 'city-dump')]" --data=http://moovitapp.com/cities/ | tee Moovit_Cities.csv
-wc -l < Moovit_Cities.csv
+OUTPUT_FILE="Moovit_Cities.csv"
+echo "City" > "$OUTPUT_FILE"
+xidel -q -e "//*/div/article/ul/li[47]/span[contains(@class, 'city-dump')]" --data=http://moovitapp.com/cities/ | tee -a "$OUTPUT_FILE"
+tail -n +2 "$OUTPUT_FILE" | wc -l
+
+# cat "$OUTPUT_FILE" | awk -F'\n' '{print "\"" $1 "\""; }' | tr '\n' ','
