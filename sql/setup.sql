8b2b126a4a9b11a4a215ac2df871e781a46e4918
Alex Butum <alexbutum@linux.com>
2016-11-01 17:52:48 +0200

    Show postgis version and all extensions at setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,9 +1,12 @@
+\c public_transport
+
 -- verify available extensions
 SELECT name, default_version,installed_version 
-FROM pg_available_extensions WHERE name LIKE 'postgis%' ;
+FROM pg_available_extensions ORDER BY name;
 
--- install extension for spatial database mygisdb
-\c public_transport
+SELECT PostGIS_full_version();
+
+-- install extension for spatial database
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 CREATE EXTENSION fuzzystrmatch;
